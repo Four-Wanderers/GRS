ff5bb4b9e596568f59e2ec7ead1e496f019d0547
Govind-Asawa <govindasawa23@gmail.com>
2020-03-15 01:57:31 +0530

    Successfully implemented assignHOD and addDept features using popper

--- admin/controller.php
+++ admin/controller.php
@@ -22,4 +22,32 @@ elseif($action == 'removeHOD')
     echo $ack;
 }
 
+elseif($action == 'addDept')
+{
+    header("Content-Type: text/plain");
+    $dept_name = $_REQUEST['dept_name'];
+    
+    $ack = (new AdminDAOImpl())->addDept($dept_name);
+    echo $ack;
+}
+
+elseif($action == 'assignhod')
+{
+    header("Content-Type: text/plain");
+    $uname = $_REQUEST['username'];
+    $email = $_REQUEST['email'];
+    $dept_name = $_REQUEST['dept_name'];
+    
+    $pass = genPass(10);
+
+    $ack = (new AdminDAOImpl())->assignHOD($uname,$email,$pass,$dept_name);
+    echo $ack;
+}
+
+function genPass(int $len):string
+{
+    $data = '1234567890abcdefghijklmnopqrstuvwxyzALSKDJFPQOWIEURYTMZNXBCV';
+    return substr(str_shuffle($data),0,$len);
+}
+
 ?>
--- admin/dept.js
+++ admin/dept.js
@@ -21,33 +21,99 @@ var callback = (url,func)=>{
     xhttp.send();
 };
 
+function setup(type)
+{
+    switch(type)
+    {
+        case 'add_dept':
+            dept_name = document.getElementById('dept_name').value;
+            url = CONTROLLER_LINK+'addDept&dept_name='+dept_name;
+            callback(url,addDept);
+            break;
+        case 'assign_hod':
+            dept_name = document.getElementById('hod_dept_name').value;
+            email = document.getElementById('hod_email').value;
+            username = document.getElementById('hod_username').value;
+            url = CONTROLLER_LINK+`assignhod&username=${username}&email=${email}&dept_name=${dept_name}`;
+            callback(url,assignHOD);
+            break;
+        case 'load':
+            callback(CONTROLLER_LINK+"departments",loadTable);
+            break;
+    }
+}
+
+function assignHOD(xhttp)
+{
+    span = document.getElementById('assignhod_ack');
+    console.log(xhttp.responseText);
+    if(xhttp.responseText)
+    {
+        // btn = document.getElementById("add_dept_btn");  
+        span.innerHTML = "assigned successfully";
+        span.setAttribute('class','text-success');
+        // btn.setAttribute('data-dismiss','modal');
+        setup('load');
+    }
+    else
+    {
+        span.innerHTML = "failed to assign or couldn't send email";
+        span.setAttribute('class','text-danger');
+    }
+}
+
+function addDept(xhttp)
+{
+    span = document.getElementById('addDept_ack');
+    if(xhttp.responseText)
+    {
+        span.innerHTML = "created successfully";
+        span.setAttribute('class','text-success');
+        setup('load');
+    }
+    else
+    {
+        span.innerHTML = "failed";
+        span.setAttribute('class','text-danger');
+    }
+}
+
+
+
 function removeHOD(xhttp){
     if(xhttp.responseText)
-        callback(CONTROLLER_LINK+"departments",loadTable);
+        setup('load');
     else
     {
         console.log('failed');
     }
 }
+function passDeptname(dept_name)
+{
+    document.getElementById("hod_dept_name").value = dept_name;
+};
 
 function loadTable(xhttp)
 {
     var table;
+    var makeEle = (type,id)=>{
+        e = document.createElement(type);
+        e.setAttribute('id',id);
+    };
+
     var makeThead = (table)=>{
                 
         tr = document.createElement("tr");
-        th = document.createElement("th");
-        th.setAttribute("id","Department");
+        
+        th = makeEle('th','Department');
         th.innerHTML = "Department";
         tr.appendChild(th);
-    
-        th = document.createElement("th");
-        th.setAttribute("id","HOD");
+        
+        th = makeEle('th','HOD');
         th.innerHTML = "HOD";
         tr.appendChild(th);
         
-        th = document.createElement("th");
-        th.setAttribute("id","Action");
+        th = makeEle('th','Action');
         th.innerHTML = "Action";
         tr.appendChild(th);
         
@@ -70,23 +136,18 @@ function loadTable(xhttp)
         td = document.createElement("td");
         if(dept.hod_name == null)
         {
-            form = document.createElement("form");
-            form.setAttribute("action",ASSIGN_HOD_LINK);
-            
-            input = document.createElement("input");
-            input.setAttribute("type","hidden");
-            input.setAttribute("value",dept.dept_name);
-            form.appendChild(input);
             
             input = document.createElement("input");
             input.setAttribute("type","submit");
             input.setAttribute("value","Assign");
-            form.appendChild(input);
-            td.appendChild(form);
+            input.setAttribute("onclick",`passDeptname('${dept.dept_name}')`);
+            input.setAttribute("data-toggle","modal");
+            input.setAttribute("data-target","#assignhod");
+            input.setAttribute("class","btn btn-primary launchConfirm pull-right ");
+            td.appendChild(input);
         }
         else
         {
-            //name of the hod
             td.innerHTML = dept.hod_name;
         }   
         tr.appendChild(td);
@@ -104,25 +165,25 @@ function loadTable(xhttp)
     };
 
     data = JSON.parse(xhttp.responseText); //array of {dept_id: -, dept_name:"",hod_name:""}
-    departments = document.getElementById("departments");
-    while(departments.firstChild)
-        departments.removeChild(departments.firstChild);
-    // departments.removeChild(departments.firstChild); //delete previous table
+    table = document.getElementById("depTable");
+    
+    while(table.firstChild)
+        table.removeChild(table.firstChild);
+    
     if(data.length === 0)
     {
-        departments.innerHTML = "No departments ";
+        document.getElementById("addcustom").innerHTML = "No Departments";
     }
     else
     {
-        table = document.createElement("table");
-        table.setAttribute("id","table");
+        document.getElementById("addcustom").innerHTML = "";
+        
         makeThead(table);
         
         for(dept of data)
         {
             makeTbody(dept, table);
         }
-        departments.appendChild(table);
     }
 
 }
